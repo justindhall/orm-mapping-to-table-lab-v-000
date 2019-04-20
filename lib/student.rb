0fffd5ca47ff5fa2bfbb7bc404e02523114aefa2
Justin Hall <hall.justin.d@gmail.com>
2019-04-20 17:04:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,12 +4,12 @@ class Student
   #  with DB[:conn]
 
   attr_accessor :name, :grade
-  attr_reader :id 
+  attr_reader :id
 
   def initialize(name, grade, id=nil)
     @name = name
     @grade = grade
-    @id = id 
+    @id = id
     @all << self
-  
+
 end
